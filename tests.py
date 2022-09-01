09abdfab2215054b5ffd398a7a14a56f072432d4
Jie Sing <44879426+JieSing@users.noreply.github.com>
2022-09-01 14:54:06 -0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -9,5 +9,18 @@ def test_it():
 	'''
 	Write a simple test.
 	'''
+	
+	s = Stack()
+	assert size = s.size()
+	s.push(1)
+	assert size = s.size()
+
+	for i in range(100):
+	  s.push(i)
+
+	assert
+	#whos
 
 	raise NotImplementedError('Need to implement a simple test.')
+	
+	
